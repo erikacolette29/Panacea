1ba6dd11654c40fe66e345c43637f85af0a5e9c9
erikacolette <erikatidwell.c@gmail.com>
2021-02-25 15:32:40 -0500

    database connect works

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 
 require('dotenv').config();
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 
